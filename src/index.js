e6defb3f563ca97fd2bf266031bbdfe85d528ffd
EdRafCas <Eduardoo.rafael@gmail.com>
2021-06-17 23:01:47 -0300

    creating repository and starting the app

--- src/index.js
+++ src/index.js
@@ -2,16 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import WebFont from 'webfontloader'
 
-ReactDOM.render(
-  <React.StrictMode>
+WebFont.load({
+  google: {
+    families: ['Work Sans:400,500,700', 'sans-serif']
+  }
+});
+
+const Index = () => {
+  return ( 
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+   );
+}
+ 
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<Index />, document.getElementById('root'));
